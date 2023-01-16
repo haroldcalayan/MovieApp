040255a5b6205f8ff5a9a6b0348565615d140935
Harold Calayan <haroldcalayan@gmail.com>
2023-01-16 21:25:10 +0800

    :sparkles: Create models

---
+++ app/src/main/java/com/haroldcalayan/movieapp/data/model/MovieItem.kt
@@ -0,0 +1,13 @@
+package com.haroldcalayan.movieapp.data.model
+
+data class MovieItem(
+    val id: Int,
+    val title: String,
+    val imageId: Int,
+    val description: String,
+    val rating: Double,
+    val duration: String,
+    val genre: String,
+    val releaseDate: String,
+    val trailerLink: String
+)
