46130c712edd7d794eedc586b3a7dae6e9a506d0
Harold Calayan <haroldcalayan@gmail.com>
2023-01-18 16:17:51 +0800

    :sparkles: Implement movie details use case

--- app/src/main/java/com/haroldcalayan/movieapp/presentation/movie_details/MovieDetailsActivity.kt
+++ app/src/main/java/com/haroldcalayan/movieapp/presentation/movie_details/MovieDetailsActivity.kt
@@ -1,12 +1,45 @@
 package com.haroldcalayan.movieapp.presentation.movie_details
 
+import android.widget.TextView
 import androidx.activity.viewModels
 import com.haroldcalayan.movieapp.R
 import com.haroldcalayan.movieapp.common.base.BaseActivity
 import com.haroldcalayan.movieapp.databinding.ActivityMovieDetailsBinding
+import com.haroldcalayan.movieapp.presentation.main.MainActivity.Companion.MOVIE_ID
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieDetailsActivity : BaseActivity<MovieDetailsViewModel, ActivityMovieDetailsBinding>() {
 
     override val layoutId = R.layout.activity_movie_details
     override val viewModel: MovieDetailsViewModel by viewModels()
+
+    override fun initViews() {
+        super.initViews()
+        val movieId = intent.getIntExtra(MOVIE_ID, 0)
+        viewModel.getMovieDetails(movieId)
+
+        toolbarAction()
+
+    }
+
+    private fun toolbarAction() {
+        binding.movieToolbarContainer.findViewById<TextView>(R.id.back_icon).setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun observe() {
+        super.observe()
+        viewModel.movieDetail.observe(this, {
+            it?.let { movieItem ->
+                binding.textviewDetailsName.text = movieItem.title
+                binding.imageviewDetailsPoster.setImageResource(movieItem.imageId)
+                binding.textviewRatings.text = movieItem.rating.toString()
+                binding.textviewShortDescription.text = movieItem.description
+                binding.textviewGenre.text = movieItem.genre
+                binding.textviewReleaseDate.text = movieItem.releaseDate
+            }
+        })
+    }
 }
--- app/src/main/java/com/haroldcalayan/movieapp/presentation/movie_details/MovieDetailsViewModel.kt
+++ app/src/main/java/com/haroldcalayan/movieapp/presentation/movie_details/MovieDetailsViewModel.kt
@@ -1,6 +1,24 @@
 package com.haroldcalayan.movieapp.presentation.movie_details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.haroldcalayan.movieapp.common.base.BaseViewModel
+import com.haroldcalayan.movieapp.data.model.MovieItem
+import com.haroldcalayan.movieapp.domain.use_case.GetMovieDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MovieDetailsViewModel: BaseViewModel() {
+@HiltViewModel
+class MovieDetailsViewModel  @Inject constructor(
+    private val getMovieDetailUseCase: GetMovieDetailUseCase
+) : BaseViewModel() {
+
+    private val _movieDetail = MutableLiveData<MovieItem?>()
+    val movieDetail: LiveData<MovieItem?> = _movieDetail
+
+    fun getMovieDetails(id: Int) {
+        invoke {
+            _movieDetail.postValue(getMovieDetailUseCase(id).data)
+        }
+    }
 }
